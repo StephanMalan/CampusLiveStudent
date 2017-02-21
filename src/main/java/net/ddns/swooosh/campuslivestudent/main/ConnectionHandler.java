811b089e25d6fc5b0ddb26f01dc05eb56902edf5
Stephan <stephanmalan.rob@gmail.com>
2017-02-21 21:00:59 +0200

    WIP

--- src/main/java/net/ddns/swooosh/campuslivestudent/main/ConnectionHandler.java
+++ src/main/java/net/ddns/swooosh/campuslivestudent/main/ConnectionHandler.java
@@ -1,4 +1,19 @@
 package net.ddns.swooosh.campuslivestudent.main;
 
 public class ConnectionHandler {
+
+    public ConnectionHandler() {
+
+    }
+
+    public Boolean authorise(String studentNumber, String password) {
+        Boolean result = studentNumber.equals("DV2015-0073") && password.equals("password");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 }
--- src/main/java/net/ddns/swooosh/campuslivestudent/main/Display.java
+++ src/main/java/net/ddns/swooosh/campuslivestudent/main/Display.java
@@ -1,13 +1,14 @@
 package net.ddns.swooosh.campuslivestudent.main;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -18,13 +19,15 @@ import javafx.stage.Stage;
 
 public class Display extends Application{
 
-    //test
     private ConnectionHandler connectionHandler = new ConnectionHandler();
+    private volatile BooleanProperty running = new SimpleBooleanProperty(false);
+    private volatile BooleanProperty result = new SimpleBooleanProperty(false);
     private Stage stage;
     private ImageView loginLogoImageView;
     private TextField studentNumberTextField;
     private PasswordField passwordField;
     private Hyperlink forgotPasswordHyperlink;
+    private ProgressIndicator waitIndicator;
     private Button loginButton;
     private VBox loginPane;
 
@@ -46,56 +49,106 @@ public class Display extends Application{
 
         //Setup Login pane
         loginLogoImageView = new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("CLLogo.png")));
-        loginLogoImageView.setFitHeight(200);
-        loginLogoImageView.setFitWidth(200);
+        loginLogoImageView.setFitHeight(250);
+        loginLogoImageView.setFitWidth(250);
         studentNumberTextField = new TextField();
         studentNumberTextField.setPromptText("Student Number");
         studentNumberTextField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -40%);" +
                 " -fx-font-size: 12pt;" +
                 " -fx-text-fill: black;" +
-                " -fx-font-family: \"Arial\";" +
+                " -fx-font-family: \"Verdana\";" +
                 " -fx-font-weight: bold;" +
                 " -fx-background-color: linear-gradient(#ffffff, #d3d3d3);" +
-                " -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
-                " -fx-background-radius: 15, 15, 15, 15;" +
+                " -fx-effect: dropshadow( three-pass-box , black , 5, 0.0 , 0 , 1 );" +
+                " -fx-background-radius: 20, 20, 20, 20;" +
                 " -fx-background-image: url(\"User.png\");" +
                 " -fx-background-repeat: no-repeat;" +
-                " -fx-background-position: left center;" +
-                " -fx-background-size: auto 100%;");
+                " -fx-background-size: auto 50%;" +
+                " -fx-background-position: 5, 5, 5, 5");
         studentNumberTextField.setMaxWidth(250);
-        studentNumberTextField.setPadding(new Insets(5, 5, 5, 50));
+        studentNumberTextField.setMinHeight(40);
+        studentNumberTextField.setPadding(new Insets(5, 5, 5, 30));
         passwordField = new PasswordField();
         passwordField.setPromptText("Password");
         passwordField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -40%);" +
                 " -fx-font-size: 12pt;" +
                 " -fx-text-fill: black;" +
-                " -fx-font-family: \"Arial\";" +
+                " -fx-font-family: \"Verdana\";" +
                 " -fx-font-weight: bold;" +
                 " -fx-background-color: linear-gradient(#ffffff, #d3d3d3);" +
-                " -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
-                " -fx-background-radius: 15, 15, 15, 15;" +
+                " -fx-effect: dropshadow( three-pass-box , black , 5, 0.0 , 0 , 1 );" +
+                " -fx-background-radius: 20, 20, 20, 20;" +
                 " -fx-background-image: url(\"Password.png\");" +
                 " -fx-background-repeat: no-repeat;" +
-                " -fx-background-position: left center;" +
-                " -fx-background-size: auto 100%;");
+                " -fx-background-size: auto 50%;" +
+                " -fx-background-position: 5, 5, 5, 15");
         passwordField.setMaxWidth(250);
-        passwordField.setPadding(new Insets(5, 5, 5, 50));
+        passwordField.setMinHeight(40);
+        passwordField.setPadding(new Insets(5, 5, 5, 30));
         loginButton = new Button("Login");
-        loginButton.setStyle("-fx-font-size: 14pt; -fx-text-fill: white; -fx-font-family: \"Arial\"; -fx-font-weight: bold; -fx-background-color: #4287a7; -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 ); -fx-background-radius: 15, 15, 15, 15;");
+        loginButton.setStyle("-fx-font-size: 16pt;" +
+                " -fx-text-fill: white;" +
+                " -fx-font-family: \"Verdana\";" +
+                " -fx-font-weight: bold;" +
+                " -fx-background-color: #4287a7;" +
+                " -fx-effect: dropshadow( three-pass-box , black , 5, 0.0 , 0 , 1 );" +
+                " -fx-background-radius: 20, 20, 20, 20;");
         loginButton.setMaxWidth(250);
+        loginButton.setMinHeight(40);
+        loginButton.setPadding(new Insets(2, 2, 2, 2));
+        loginButton.setOnAction((ActionEvent e) -> {
+           if (studentNumberTextField.getText().length() < 11) { //TODO min length login
+               //TODO
+           } else if (passwordField.getText().length() < 5) { //TODO min length password
+               //TODO
+           } else {
+               loginPane.getChildren().clear();
+               loginPane.getChildren().addAll(loginLogoImageView, waitIndicator);
+               running.setValue(true);
+               Thread loginThread = new Thread(() -> {
+                    result.setValue(connectionHandler.authorise(studentNumberTextField.getText(), passwordField.getText()));
+                    running.set(false);
+               });
+               loginThread.start();
+               running.addListener(al -> {
+                   if (!running.getValue()) {
+                       if (result.getValue()) {
+                           Platform.runLater(() -> {
+                               contentPane.getChildren().clear();
+                               contentPane.getChildren().addAll(backgroundPane);
+                           });
+                       } else {
+                           Platform.runLater(() -> {
+                               loginPane.getChildren().clear();
+                               loginPane.getChildren().addAll(loginLogoImageView, studentNumberTextField, passwordField, loginButton, forgotPasswordHyperlink);
+                               passwordField.clear();
+                               passwordField.requestFocus();
+                           });
+                       }
+                   }
+               });
+           }
+        });
+        loginButton.setDefaultButton(true);
         forgotPasswordHyperlink = new Hyperlink("Forgot Password?");
-        forgotPasswordHyperlink.setFont(new Font(16));
+        forgotPasswordHyperlink.setFont(new Font("Verdana", 14));
+        waitIndicator = new ProgressIndicator(ProgressIndicator.INDETERMINATE_PROGRESS);
+        waitIndicator.setPrefSize(200, 200);
         loginPane = new VBox(loginLogoImageView, studentNumberTextField, passwordField, loginButton, forgotPasswordHyperlink);
         loginPane.setAlignment(Pos.CENTER);
         loginPane.setSpacing(15);
         loginPane.setPadding(new Insets(10));
         loginPane.setMaxSize(500, 500);
-        //loginPane.setStyle("-fx-border-color: black;  -fx-background-color: white");
 
         //Setup background pane
         backgroundPane = new StackPane();
-        backgroundPane.setStyle("-fx-background-image: url(\"Background.png\"); -fx-background-size: auto 100%; -fx-background-position: center; -fx-background-repeat: no-repeat;-fx-background-color: white;-fx-accent: white");
-        backgroundPane.setEffect(new GaussianBlur(25));
+        backgroundPane.setStyle("-fx-background-image: url(\"Background.png\");" +
+                " -fx-background-size: auto 100%;" +
+                " -fx-background-position: center;" +
+                " -fx-background-repeat: no-repeat;" +
+                " -fx-background-color: white;" +
+                " -fx-accent: white");
+        backgroundPane.setEffect(new GaussianBlur(150));
 
         //Setup content pane
         contentPane = new StackPane(backgroundPane, loginPane);
